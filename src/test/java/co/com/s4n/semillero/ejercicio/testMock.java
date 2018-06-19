4a6a075339f298a4ba3176c9aa08998638c5ae45
Jeniffer Florez Londoño <jeniflorezl95@gmail.com>
2018-06-19 17:17:12 -0500

    third commit

---
+++ src/test/java/co/com/s4n/semillero/ejercicio/testMock.java
@@ -0,0 +1,15 @@
+package co.com.s4n.semillero.ejercicio;
+
+import io.vavr.collection.List;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.*;
+
+
+public class testMock {
+
+    @Mock
+    List<String> lista = List.of("Jeniffer");
+
+
+}
